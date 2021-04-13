8096cdcdc39ebb951e1358d423a5e66c105f3fdb
Seitek <dzh.seitek@gmail.com>
2021-04-13 21:38:24 +0600

    the code's been rewritten using functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-const numberOfFilms = +prompt('How many movies have you watched?', '');
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt('How many movies have you watched?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('How many movies have you watched?', '');
+  }
+}
+// start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,27 +17,45 @@ const personalMovieDB = {
   private: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const recentlyWatchedMovie = prompt('What movie have you watched recently?', ''),
-        rateRecentlyWatchedMovie = +prompt('How would your rate the movie?', '');
+function rememberMyMovies() {
+  for (let i = 0; i < 2; i++) {
+    const recentlyWatchedMovie = prompt('What movie have you watched recently?', ''),
+          rateRecentlyWatchedMovie = +prompt('How would your rate the movie?', '');
+  
+    if (recentlyWatchedMovie != null && rateRecentlyWatchedMovie != null && recentlyWatchedMovie != '' && rateRecentlyWatchedMovie != '' && recentlyWatchedMovie.length < 50) {
+      personalMovieDB.movies[recentlyWatchedMovie] = rateRecentlyWatchedMovie;
+      console.log('DONE');
+    } else {
+      i--;
+      console.log('ERROR');
+    }
+  }
+}
+// rememberMyMovies();
 
-  if (recentlyWatchedMovie != null && rateRecentlyWatchedMovie != null && recentlyWatchedMovie != '' && rateRecentlyWatchedMovie != '' && recentlyWatchedMovie.length < 50) {
-    personalMovieDB.movies[recentlyWatchedMovie] = rateRecentlyWatchedMovie;
-    console.log('DONE');
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('low');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('classic');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('dumbass');
   } else {
-    i--;
     console.log('ERROR');
   }
 }
+// detectPersonalLevel();
 
-if (personalMovieDB.count < 10) {
-  console.log('low');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-  console.log('classic');
-} else if (personalMovieDB.count > 30) {
-  console.log('dumbass');
-} else {
-  console.log('ERROR');
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
 }
+showMyDB(personalMovieDB.private);
 
-console.log(personalMovieDB);
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Your favourite genre ${i}`);;
+  }
+}
+writeYourGenres();
