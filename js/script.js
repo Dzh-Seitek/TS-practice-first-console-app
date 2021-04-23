ebb64eaa8755540665215f7efec270d237eb4af4
Seitek <dzh.seitek@gmail.com>
2021-04-23 20:44:59 +0600

    rewrote functions into methods of personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,61 +1,72 @@
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt('How many movies have you watched?', '');
-
-  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('How many movies have you watched?', '');
-  }
-}
-// start();
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   private: false,
-};
-
-function rememberMyMovies() {
-  for (let i = 0; i < 2; i++) {
-    const recentlyWatchedMovie = prompt('What movie have you watched recently?', ''),
-          rateRecentlyWatchedMovie = +prompt('How would your rate the movie?', '');
+  start: function() {
+      personalMovieDB.count = +prompt('How many movies have you watched?', '');
+    
+      while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+        personalMovieDB.count = +prompt('How many movies have you watched?', '');
+      }
+  },
+  rememberMyMovies: function () {
+    for (let i = 0; i < 2; i++) {
+      const recentlyWatchedMovie = prompt('What movie have you watched recently?', ''),
+            rateRecentlyWatchedMovie = +prompt('How would your rate the movie?', '');
   
-    if (recentlyWatchedMovie != null && rateRecentlyWatchedMovie != null && recentlyWatchedMovie != '' && rateRecentlyWatchedMovie != '' && recentlyWatchedMovie.length < 50) {
-      personalMovieDB.movies[recentlyWatchedMovie] = rateRecentlyWatchedMovie;
-      console.log('DONE');
+      if (recentlyWatchedMovie != null && rateRecentlyWatchedMovie != null && recentlyWatchedMovie != '' && rateRecentlyWatchedMovie != '' && recentlyWatchedMovie.length < 50) {
+        personalMovieDB.movies[recentlyWatchedMovie] = rateRecentlyWatchedMovie;
+        console.log('DONE');
+      } else {
+        i--;
+        console.log('ERROR');
+      }
+    }
+  },
+  detectPersonalLevel: function () {
+    if (personalMovieDB.count < 10) {
+      console.log('low');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log('classic');
+    } else if (personalMovieDB.count >= 30) {
+      console.log('dumbass');
     } else {
-      i--;
       console.log('ERROR');
     }
-  }
-}
-// rememberMyMovies();
-
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log('low');
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('classic');
-  } else if (personalMovieDB.count >= 30) {
-    console.log('dumbass');
-  } else {
-    console.log('ERROR');
-  }
-}
-// detectPersonalLevel();
-
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
-showMyDB(personalMovieDB.private);
-
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    personalMovieDB.genres[i - 1] = prompt(`Your favourite genre ${i}`);;
-  }
-}
-writeYourGenres();
+  },
+  writeYourGenres: function () {
+    for (let i = 1; i < 2; i++) {
+      // let genre = prompt(`Your favourite genre ${i}`);
+      // if (genre === '' || genre === null) {
+      //   console.log('Incorrect/missing data');
+      //   i--;
+      // } else {
+      //   personalMovieDB.genres[i - 1] = genre;
+      // }
+      let genres = prompt('Enter favorite genres with ","');
+      if (genres === '' || genres === null) {
+        console.log('Incorrect/missing data');
+        i--;
+      } else {
+        personalMovieDB.genres = genres.split(', ');
+      }
+    }
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Favourite genre ${i + 1} - ${item}`);
+    });
+  },
+  showMyDB: function(hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+  toggleVisibleMyDB: function() {
+    if (personalMovieDB.private) {
+      personalMovieDB.private = false;
+    } else {
+      personalMovieDB.private = true;
+    }
+  },
+};
